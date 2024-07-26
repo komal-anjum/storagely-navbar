f15317a96db42fd27e156e0700a880f3e1da1abe
komal-anjum <156304938+komal-anjum@users.noreply.github.com>
2024-07-26 20:07:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,46 +3,75 @@ $(document).ready(function() {
     $('#navbar-menu-icon').click(function() {
         $(this).toggleClass('open');
         $('#navbar-menu').toggleClass('open');
+        
+       
+
+       // Check if navbar-menu-icon has the open class
+     if (!$(this).hasClass('open')) {
+        $('.mobile-menu').removeClass('active');
+        $('.mobile-navbar-container').css('display', 'none');
+
+    }
+  
     });
 
-    // Toggle dropdowns on click for mobile
-    $('.navbar-item.dropdown > .navbar-link, .dropdown-item.dropdown > .dropdown-link').click(function(e) {
-        if ($(window).width() <= 1160) {
-            e.preventDefault();
-
-            var $dropdown = $(this).parent();
-            var $subMenu = $dropdown.children('.dropdown-menu');
-            var $icon = $(this).find('.mobile-plus-minus');
-
-            // Toggle the dropdown menu and icon
-            if ($dropdown.hasClass('open')) {
-                $dropdown.removeClass('open');
-                $subMenu.slideUp();
-                $icon.removeClass('fa-minus').addClass('fa-plus');
-            } else {
-                $dropdown.addClass('open');
-                $subMenu.slideDown();
-                $icon.removeClass('fa-plus').addClass('fa-minus');
-            }
-        }
-    });
-
-    // Close dropdowns when clicking outside
-    $(document).click(function(e) {
-        if ($(window).width() <= 768 && !$(e.target).closest('.navbar').length) {
-            $('.dropdown').removeClass('open');
-            $('.dropdown-menu').slideUp();
-            $('.mobile-plus-minus').removeClass('fa-minus').addClass('fa-plus');
-        }
-    });
 
     // Prevent dropdown closing when clicking inside the menu
     $('.dropdown-menu').click(function(e) {
         e.stopPropagation();
     });
 
-    // Initialize all dropdown icons to plus by default
-    $('.dropdown-menu').each(function() {
-        $(this).siblings('.dropdown-link').find('.mobile-plus-minus').addClass('fa-plus').removeClass('fa-minus');
+
+    $(document).ready(function() {
+        $('.navbar-item.dropdown').click(function() {
+            $('.mobile-menu').addClass('active');
+        });
     });
+
+    
+});
+
+
+$(document).ready(function() {
+    // Show dropdown content in mobile-navbar-container
+    $('.navbar-link').click(function(e) {
+        e.preventDefault(); // Prevent the default action
+
+        var targetId = $(this).data('target');
+        var content = $('#' + targetId).html();
+        $('.mobile-navbar-container').html(content).slideDown();
+
+        // Re-attach event handler for newly added dropdown links
+        attachDropdownHandlers();
+    });
+
+    function attachDropdownHandlers() {
+        // Unbind any existing handlers to prevent duplicate handling
+        $('.mobile-navbar-container .dropdown-link').off('click');
+
+        // Handle click event on dropdown links in the mobile-navbar-container
+        $('.mobile-navbar-container .dropdown-link').on('click', function(e) {
+            e.preventDefault(); // Prevent the default action
+
+            var $this = $(this);
+            var $subMenu = $this.next('.dropdown-menu');
+            var $icon = $this.find('.mobile-plus-minus');
+
+            // Toggle the visibility of the dropdown menu
+            if ($subMenu.is(':visible')) {
+                $subMenu.slideUp();
+                $icon.removeClass('fa-minus').addClass('fa-plus');
+            } else {
+                $subMenu.slideDown();
+                $icon.removeClass('fa-plus').addClass('fa-minus');
+            }
+
+            // Close other open submenus and reset their icons
+            $subMenu.parent().siblings().find('.dropdown-menu').slideUp();
+            $subMenu.parent().siblings().find('.mobile-plus-minus').removeClass('fa-minus').addClass('fa-plus');
+        });
+    }
+
+    // Initial attachment of handlers
+    attachDropdownHandlers();
 });
